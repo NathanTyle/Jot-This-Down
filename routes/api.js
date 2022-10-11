e9498ef1605fbdcf1771e5b8db25e854d83628fb
Tyler Charro <106177945+NathanTyle@users.noreply.github.com>
2022-10-11 16:23:40 -0500

    added notes.post funtionality to api.js file

--- routes/api.js
+++ routes/api.js
@@ -7,3 +7,23 @@ notes.get('/notes', (req, res) => {
     console.info(`${req.method} request received for notes`);
     readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
   });
+
+  notes.post('/notes', (req, res) => {
+    console.info(`${req.method} request received to add a notes`);
+    console.log(req.body);
+  
+    const { title, text } = req.body;
+  
+    if (req.body) {
+      const newTip = {
+        title,
+        text,
+        id: uuid(),
+      };
+  
+      readAndAppend(newTip, './db/db.json');
+      res.json(`notes added successfully 🚀`);
+    } else {
+      res.error('Error in adding notes');
+    }
+  });
